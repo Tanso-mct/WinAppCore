e8da47af2e9064add4a0a41cbef5bb28f71ac8ee
Tanso <tansomctexp@gmail.com>
2025-03-26 19:17:11 +0900

    [UPDATE]Finished creating functions for output to Console. Added tests for them.

---
+++ WinAppCore/WinAppCore/include/console_output.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "config.h"
+
+#include <iostream>
+#include <initializer_list>
+#include <string>
+#include <string_view>
+
+namespace WACore
+{
+
+WIN_APP_CORE unsigned int& ErrorCount();
+WIN_APP_CORE unsigned int& WarningCount();
+
+class WIN_APP_CORE IssueNotifier
+{
+public:
+    IssueNotifier();
+    IssueNotifier(unsigned int errorCount, unsigned int warningCount);
+    virtual ~IssueNotifier();
+};
+
+WIN_APP_CORE void Cout(std::initializer_list<std::string_view> args);
+WIN_APP_CORE void CoutErr(std::initializer_list<std::string_view> args);
+WIN_APP_CORE void CoutWrn(std::initializer_list<std::string_view> args);
+WIN_APP_CORE void CoutInfo(std::initializer_list<std::string_view> args);
+WIN_APP_CORE void CoutDebug(std::initializer_list<std::string_view> args);
+
+} // namespace WACore
